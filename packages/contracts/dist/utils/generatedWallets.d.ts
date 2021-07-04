8c7e79bf892ef6afd0abd04fcccd3e62788b5ea2
shahruz <hello@shahr.uz>
2021-07-03 17:38:33 -0700

    Revert "unignore contracts dist"
    This reverts commit b36f52c61dc409943df71e97db4c8b3e27ae4719.

--- packages/contracts/dist/utils/generatedWallets.d.ts
+++
@@ -1,4 +0,0 @@
-import { ethers } from 'ethers';
-export declare const privateKeys: string[];
-export declare const generatedWallets: (provider: ethers.providers.BaseProvider) => ethers.Wallet[];
-export declare const signMessage: (message: string, wallet: ethers.Wallet) => Promise<Uint8Array>;
