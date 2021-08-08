0a1b51641a80691e8b9f29cac8b28c2785db88cd
shahruz <hello@shahr.uz>
2021-08-08 15:07:19 -0700

    dependency updates

--- packages/website/src/pages/_app.tsx
+++ packages/website/src/pages/_app.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
 import MainLayout from '@app/layouts/MainLayout';
-//@ts-ignore
-import { UseWalletProvider } from 'use-wallet';
+import PlausibleProvider from 'next-plausible';
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <UseWalletProvider
-      chainId={1}
-      connectors={{
-        walletconnect: {
-          rpcUrl:
-            'https://mainnet.infura.io/v3/31cab49b254143188fc112a0c332ad86'
-        }
-      }}
-    >
+    <PlausibleProvider domain="internet.camera">
       <MainLayout>
         <Component {...pageProps} />
       </MainLayout>
-    </UseWalletProvider>
+    </PlausibleProvider>
   );
 }
 
