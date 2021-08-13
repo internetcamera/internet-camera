ab60872a102ef9f25d7151d26f045ff6a98abd30
shahruz <hello@shahr.uz>
2021-08-13 11:12:06 -0700

    remove hooks library from site for now

--- packages/website/src/pages/_app.tsx
+++ packages/website/src/pages/_app.tsx
@@ -2,16 +2,13 @@ import React from 'react';
 import type { AppProps } from 'next/app';
 import MainLayout from '@app/layouts/MainLayout';
 import PlausibleProvider from 'next-plausible';
-import InternetCamera from '@internetcamera/react-hooks';
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <PlausibleProvider domain="internet.camera" exclude="/utils/rn-wallet">
-      <InternetCamera.Provider>
-        <MainLayout>
-          <Component {...pageProps} />
-        </MainLayout>
-      </InternetCamera.Provider>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
     </PlausibleProvider>
   );
 }
