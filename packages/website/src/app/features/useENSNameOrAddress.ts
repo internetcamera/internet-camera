3b03ee01dc04d5bfef1b2530e236a183471d72e6
shahruz <hello@shahr.uz>
2021-11-03 15:06:19 -0700

    update types

--- packages/website/src/app/features/useENSNameOrAddress.ts
+++ packages/website/src/app/features/useENSNameOrAddress.ts
@@ -1,14 +1,14 @@
-import { InternetCameraAddresses } from "@internetcamera/sdk";
-import { providers } from "ethers";
-import { persist } from "zustand/middleware";
-import create from "zustand";
-import { useEffect } from "react";
+import { InternetCameraAddresses } from '@internetcamera/sdk';
+import { providers } from 'ethers';
+import { persist } from 'zustand/middleware';
+import create from 'zustand';
+import { useEffect } from 'react';
 
 const getENSName = async (account: string) => {
   if (
     account.toLowerCase() == InternetCameraAddresses[80001].camera.toLowerCase()
   )
-    return "Internet Camera";
+    return 'Internet Camera';
   return new providers.JsonRpcProvider(
     `https://mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_ID}`,
     1
@@ -16,22 +16,22 @@ const getENSName = async (account: string) => {
 };
 
 type ENSStoreState = {
-  addressBook: Record<string, string>;
+  addressBook: Record<string, string | null>;
 };
 
 export const useENSStore = create<ENSStoreState>(
   persist(
     (set, get) => ({
-      addressBook: {},
+      addressBook: {}
     }),
     {
-      name: "ens-internet-camera",
+      name: 'ens-internet-camera'
     }
   )
 );
 
 const useENSNameOrAddress = (account?: string) => {
-  const ensName = useENSStore((state) =>
+  const ensName = useENSStore(state =>
     account
       ? state.addressBook[account.toLowerCase()] || account.toLowerCase()
       : null
@@ -39,9 +39,9 @@ const useENSNameOrAddress = (account?: string) => {
   useEffect(() => {
     if (!account || !ensName) return;
     if (ensName.toLowerCase() != account.toLowerCase()) return;
-    getENSName(account).then((name) => {
-      useENSStore.setState((state) => ({
-        addressBook: { ...state.addressBook, [account.toLowerCase()]: name },
+    getENSName(account).then(name => {
+      useENSStore.setState(state => ({
+        addressBook: { ...state.addressBook, [account.toLowerCase()]: name }
       }));
     });
   }, [ensName]);
